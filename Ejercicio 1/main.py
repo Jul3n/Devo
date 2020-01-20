6be37da5812915fa0b26e5eac36756c9bc7c5441
Jul3n <41755774+Jul3n@users.noreply.github.com>
2020-01-20 16:24:53 +0100

    Update main.py
    print("Los valores menos a cero serán calculados en su valor absoluto.")

--- Ejercicio 1/main.py
+++ Ejercicio 1/main.py
@@ -5,6 +5,7 @@ from array import *
 Vector=[1, 2, 3, 4, 5, 6, 28, 496, 8128, 12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100, 102]
 
 suma=0 #Inicialiación de la suma a cero.
+print("Los valores menos a cero serán calculados en su valor absoluto.")
 for valor in Vector: #Recorremos todo el vector de prueba.
     suma=sumar_divisores(valor) #Obtenemos la suma de todos los divisores del valor en la posición "i".
     if suma < valor: #Si la suma es menor al valor es DEFECTIVO.
