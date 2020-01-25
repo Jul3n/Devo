c5d02e9400035eec7f1adbd275fa4f1155d9582b
Jul3n <41755774+Jul3n@users.noreply.github.com>
2020-01-25 23:19:27 +0100

    Update main.py
    abs() update.

--- Ejercicio 1/main.py
+++ Ejercicio 1/main.py
@@ -2,15 +2,15 @@ from sumar_divisores import *
 from array import *
 
 #Vector de valores (obtenidos de Wikipedia) para realizar el test.
-Vector=[1, 2, 3, 4, 5, 6, 28, 496, 8128, 12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100, 102]
+Vector=[-3, -4, 0, 1, 2, 3, 4, 5, 6, 28, 496, 8128, 12, 18, 20, -5, -1, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72, 78, 80, 84, 88, 90, 96, 100, 102]
 
 suma=0 #Inicialiación de la suma a cero.
-print("Los valores menos a cero serán calculados en su valor absoluto.")
+print("Los valores menores a cero serán calculados en su VALOR ABSOLUTO.")
 for valor in Vector: #Recorremos todo el vector de prueba.
     suma=sumar_divisores(valor) #Obtenemos la suma de todos los divisores del valor en la posición "i".
-    if suma < valor: #Si la suma es menor al valor es DEFECTIVO.
+    if suma < abs(valor): #Si la suma es menor al valor es DEFECTIVO.
         print("El valor ", valor, "es DEFECTIVO")
-    elif suma == valor: #Si la suma es igual al valor es PERFECTO.
+    elif suma == abs(valor): #Si la suma es igual al valor es PERFECTO.
         print("El valor ", valor, "es PERFECTO")
     else: # suma > valor #Si la suma es mayor es ABUNDANTE.
         print("El valor ", valor, "es ABUNDANTE")
